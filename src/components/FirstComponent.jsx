3ae683abb8f57ed05988c12d48d4d78799ec3f9f
Luiz Bueno <luizbueno.dev@gmail.com>
2026-04-16 17:24:32 -0300

    add component with React Fragment

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,10 @@
+function FirstComponent(){
+    return(
+        <>
+            <h1>Hello World!</h1>
+            <p>Learning to use the fragment</p>
+        </>
+    );
+}
+
+export default FirstComponent;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+//Import creatRoot
+import { createRoot } from "react-dom/client";
+//Import components
+import FirstComponent from "./components/FirstComponent";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//Show components
+createRoot(document.getElementById("root")).render(
+    <FirstComponent />
+);
