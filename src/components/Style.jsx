fbd08d2741dcd1a53417f6be1d022a7757d1098c
Luiz Bueno <luizbueno.dev@gmail.com>
2026-04-20 15:00:22 -0300

    add style

---
+++ src/components/Style.jsx
@@ -0,0 +1,16 @@
+//Import style
+import "./Style.css";
+
+//Component
+function Style(){
+    return(
+        <>
+            <p className="blue">Paragraph 1</p>
+            <p className="green">Paragraph 2</p>
+            <p className="red">Paragraph 3</p>
+        </>           
+    );
+}
+
+//Export
+export default Style;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from "react-dom/client";
 //Import components
 import FirstComponent from "./components/FirstComponent";
 import Image from "./components/Image";
+import Style from "./components/Style";
 
 //Show components
 createRoot(document.getElementById("root")).render(
-    <Image />
+    <Style />
 );
